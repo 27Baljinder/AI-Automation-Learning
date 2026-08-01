ea8ef2ea6ab78141a884f00fa7b2c51492228015
Baljinder Sidhu <baljinder90234@gmail.com>
2026-08-01 10:58:47 +0530

    exercise 1

--- Day 01/exercises.py
+++ Day 01/exercises.py
@@ -1,1 +1,6 @@
-print("hello, world!")
+name = "Baljinder Singh"
+age = 27
+weight = 84.5
+height = 5.11
+reason = "I want to learn Python for AI automation."
+print("My name is", name, "and I am", age, "years old", "and i weigh", weight, "kg and my height is", height, "and",reason, ".")
