fe547e3bc7f048dbaee0f89e669138798d1d497e
Baljinder Sidhu <baljinder90234@gmail.com>
2026-08-01 11:11:55 +0530

    more tasks

--- Day 01/exercises.py
+++ Day 01/exercises.py
@@ -4,3 +4,17 @@ weight = 84.5
 height = 5.11
 reason = "I want to learn Python for AI automation."
 print("My name is", name, "and I am", age, "years old", "and i weigh", weight, "kg and my height is", height, "and",reason, ".")
+print(type(name))
+print(type(age))
+print(type(weight))
+print(type(reason))
+x = 10
+y = 3
+
+print(x + y)
+print(x - y)
+print(x * y)
+print(x / y)
+print(x % y)
+print(x // y)
+print(x ** y)
